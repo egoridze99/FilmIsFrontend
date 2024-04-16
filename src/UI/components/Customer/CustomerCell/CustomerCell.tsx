c8d7db329569552d922b989393ec93eea6357458
Egori <egoridze81@gmail.com>
2024-04-16 18:37:01 +0300

    Маркер о незаполненных паспортных данных

--- src/UI/components/Customer/CustomerCell/CustomerCell.tsx
+++ src/UI/components/Customer/CustomerCell/CustomerCell.tsx
@@ -4,17 +4,22 @@ import {Customer} from "src/types/customer.types";
 import "./customerCell.scss";
 import CustomerCard from "src/UI/components/Customer/CustomerCard";
 import {useCustomerCardProps} from "src/hooks/useCustomerCardProps";
+import {isCustomerHasBlankFields} from "src/utils/customer/isCustomerHasBlankFields";
+import {Icon, Tooltip} from "@mui/material";
+import DoDisturbOnIcon from "@mui/icons-material/DoDisturbOn";
 
 type CustomerCellProps = {
   customer: Customer;
 };
 
 const CustomerCell: React.FC<CustomerCellProps> = ({customer}) => {
+  const hasCustomerBlankFields = isCustomerHasBlankFields(customer);
+
   const {
     onCustomerEdit,
     closeCustomerCardDialog,
     openCustomerCardDialog,
-    customer: customerCopy
+    customer: isOpen
   } = useCustomerCardProps();
 
   return (
@@ -23,11 +28,22 @@ const CustomerCell: React.FC<CustomerCellProps> = ({customer}) => {
         className={"CustomerCell"}
         onClick={() => openCustomerCardDialog(customer)}
       >
+        {hasCustomerBlankFields && (
+          <Tooltip
+            title={"Незаполненные паспортные данные"}
+            className="CustomerCell__marker"
+          >
+            <Icon>
+              <DoDisturbOnIcon sx={{color: "#e31235", fontSize: "18px"}} />
+            </Icon>
+          </Tooltip>
+        )}
+
         <div>{customer.name}</div>
         <div>{customer.telephone}</div>
       </div>
       <CustomerCard
-        customer={customerCopy}
+        customer={isOpen ? customer : null}
         onEdit={onCustomerEdit}
         onClose={closeCustomerCardDialog}
       ></CustomerCard>
